d62a9deb367b9f2115f4a6066347191477be73e8
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-02 19:55:00 +0300

    Добавлено ожидание, так как инет слабый и падает тест

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 class TestButton():
     def test_button(self, browser):
+        browser.implicitly_wait(5) #Неявное ожидание, так как если инет слабый тест падает
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         button = browser.find_element(By.CLASS_NAME, 'btn8.btn-lg.btn-primary.btn-add-to-basket')
