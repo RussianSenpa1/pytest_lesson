7732392552d5a0423de203ccaa29debe4a256984
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-02 19:43:17 +0300

    Изменение названия классов и функций, добавлен assert

--- test_items.py
+++ test_items.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium.webdriver.common.by import By
 
-class Test():
-    def test_link(self, browser):
-        browser.implicitly_wait(5)
+class TestButton():
+    def test_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
-        browser.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
+        button = browser.find_element(By.CLASS_NAME, 'btn8.btn-lg.btn-primary.btn-add-to-basket')
+        assert button, 'Кнопка не найдена'
 
 if __name__ == "__main__":
     pytest.main()
