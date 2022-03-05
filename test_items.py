9cc36d7c708016689430358ee1465bef72b6c19c
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-05 11:12:03 +0300

    Исправлена опечатка в классе Button

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ class TestButton():
         browser.implicitly_wait(5) #Неявное ожидание, так как если инет слабый тест падает
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
-        button = browser.find_element(By.CLASS_NAME, 'btn8.btn-lg.btn-primary.btn-add-to-basket')
+        button = browser.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
         assert button, 'Кнопка не найдена'
 
 if __name__ == "__main__":
